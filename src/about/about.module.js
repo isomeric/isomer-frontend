da29f47aab5f988b86ddd404e9d76e370da91fae
ri0t <riot@c-base.org>
2019-07-30 21:03:02 +0200

    Refined about dialog to contain package information

--- src/about/about.module.js
+++ src/about/about.module.js
@@ -17,6 +17,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import './about-component/about.scss';
+
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
 
